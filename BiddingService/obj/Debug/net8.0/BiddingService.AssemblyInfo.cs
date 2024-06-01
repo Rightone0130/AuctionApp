451b08017b19e313e12f41044f9dd6afb9d5dec7
rightone0130 <rightone0130@gmail.com>
2024-06-01 01:51:56 -0700

    FIXED IGNORED BUG

--- BiddingService/obj/Debug/net8.0/BiddingService.AssemblyInfo.cs
+++ BiddingService/obj/Debug/net8.0/BiddingService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BiddingService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b0faa3d336f638019f39fd26f777fb93de58ce4b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a1d6a7be051b3cff4b752da7ad9c3cc2c5f58d55")]
 [assembly: System.Reflection.AssemblyProductAttribute("BiddingService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BiddingService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
